e8bec6678709703b2f510221804b230b3884d031
nGincode <fembinurilham@gmail.com>
2026-02-02 10:47:24 +0700

    [MOD] add dev/prod

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-import express from 'express'
+import express from "express";
 const app = express();
-import { request, response } from './app/middleware/logger.js';
-import * as uuid from 'uuid'
-import 'dotenv/config'
-import monit from './app/controller/monit.routes.js';
-import output from './app/controller/output/output.routes.js'
-import terminal from './app/controller/terminal/terminal.routes.js'
-import basicAuth from './app/middleware/basic-auth.js';
+import { request, response } from "./app/middleware/logger.js";
+import * as uuid from "uuid";
+import "dotenv/config";
+import monit from "./app/controller/monit.routes.js";
+import output from "./app/controller/output/output.routes.js";
+import terminal from "./app/controller/terminal/terminal.routes.js";
+import basicAuth from "./app/middleware/basic-auth.js";
+import morgan from "morgan";
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
 const originalSend = app.response.send;
 app.response.send = function sendOverWrite(body) {
-    originalSend.call(this, body);
-    this.__custombody__ = body;
+  originalSend.call(this, body);
+  this.__custombody__ = body;
 };
 
 //get request ID
 app.use((req, res, next) => {
-    req.headers.idReq = uuid.v4();
-    next();
+  req.headers.idReq = uuid.v4();
+  next();
 });
 app.use(request);
 app.use(response);
@@ -28,10 +29,11 @@ app.use(response);
 var router = express.Router();
 
 app.get("/", (req, res) => {
-    return res.status(200).send("Connect!");
+  return res.status(200).send("Connect!");
 });
 
 // app.use(basicAuth)
+process.env.NODE_ENV === "development" && app.use(morgan("dev"));
 app.use("/api/v1", router);
 router.use("/monit", monit);
 router.use("/output", output);
@@ -39,5 +41,7 @@ router.use("/terminal", terminal);
 
 const port = process.env.APP_PORT || 5000;
 app.listen(port, () => {
-    console.log(`System is listening to port ${port}`);
+  console.log(
+    `System is listening to port http://localhost:${port} (${process.env.NODE_ENV})`,
+  );
 });
