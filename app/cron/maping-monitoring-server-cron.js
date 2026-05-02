b6da4a102a755635d6b23776ebb98ee0d8ee7ecd
tyko.nutech@gmail.com <tyko.nutech@gmail.com>
2026-05-02 17:20:26 +0700

    Update

--- app/cron/maping-monitoring-server-cron.js
+++ app/cron/maping-monitoring-server-cron.js
@@ -96,7 +96,7 @@ export const runServerMonitoringCron = async () => {
                                 value: +Number(row.usage_percent).toFixed(2),
                                 status: status,
                                 measure: '%',
-                                c_data_type: `disk_usage_${row.mountpoint.replace('/', '').toLowerCase() || 'root'}`
+                                c_data_type: `DISK_USAGE_${row.mountpoint.replace('/', '').toUpperCase() || 'ROOT'}`
                             };
                             });
                         monitoringData.push(...data);
